1001c710e5e1c0429055b7ad9042a03da9dc3242
AmaniN16 <anuma1@ufl.edu>
2022-04-02 17:50:33 -0400

    Update Source.cpp

--- PageRank/Source.cpp
+++ PageRank/Source.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <algorithm>
 #include <map>
@@ -8,23 +9,28 @@
 using namespace std;
 
 struct dj {
-    int d = 0;
+    double d;
     string toS;
 
-    int axDj() {
-        return ++d;
+    double axDj() {
+        ++d;
+        return d;
     }
 };
 
-dj findDj(vector<dj> djs, string to) {
-    dj currDj;
+dj* findDj(vector<dj*> djs, string to) {
+    dj* currDj = new dj;
 
     for (int i = 0; i < djs.size(); i++) {
-        if (djs[i].toS == to)
+        if (djs[i]->toS == to)
             currDj = djs[i];
     }
 
     return currDj;
+}
+
+bool comparator(string a, string b) {
+    return a < b;
 }
 
 class AdjacencyList {
@@ -33,104 +39,147 @@ private:
     map<int, vector<pair<int, double>>>::iterator it;
 
 public:
-    void insertEdge(map<string, int> mapper, string from, dj to);
-    void PageRank(map<string, int> mapper, int p, int n);
+    void insertEdge(map<string, int> mapper, string from, dj* to);
+    void PageRank(map<string, int> mapper, vector<string> urls, int p, int n);
     
 };
 
-void AdjacencyList::insertEdge(map<string, int> mapper, string from, dj to) {
-    adjlist[mapper[from]].push_back(make_pair(mapper[to.toS], 1 / (to.axDj())));
+void AdjacencyList::insertEdge(map<string, int> mapper, string from, dj* to) {
 
-    if (adjlist.find(mapper[to.toS]) == adjlist.end())
-        adjlist[mapper[to.toS]] = {};
+    adjlist[mapper[from]].push_back(make_pair(mapper[to->toS], (1 / (to->axDj()))));
+
+    if (adjlist.find(mapper[to->toS]) == adjlist.end())
+        adjlist[mapper[to->toS]] = {};
+
+    //check for repeat to ig.
+    for (it = adjlist.begin(); it != adjlist.end(); ++it) {
+        for (int i = 0; i < it->second.size(); i++) {
+            //If theres a repeat from a different from, then update the rank
+            if (it->second[i].first == mapper[to->toS]) {
+                it->second[i].second = (1 / to->d);
+            }
+        }
+    }
+   
 }
 
 /* prints the PageRank of all pages after p powerIterations in ascending alphabetical order of webpagesand rounding rank to two
  decimal places */
-void AdjacencyList::PageRank(map<string, int> mapper, int p, int n) {
+void AdjacencyList::PageRank(map<string, int> mapper, vector<string> urls, int p, int n) {
     vector<double> ranks;
     vector<double> newRanks;
 
     //initialize initial ranks
     for (int i = 0; i < n; i++) {
-        ranks.push_back(1 / n);
+        ranks.push_back((1 / (double)n));
     }
 
+    //page rank calculation and printing
     if (p > 1) {
-        //Iterator is gonna go thru all of the rows
-        //Remember adjlist first is integer values based on mapper
-        for (it = adjlist.begin(); it != adjlist.end(); ++it) {
-            for (int i = 0; i < n; i++) {
-                if (it->second[i].second == 0)
-                    return;
+        for (int pi = 1; pi < p; pi++) {
+            //Iterator is gonna go thru all of the rows
+            //Remember adjlist first is integer values based on mapper
+            for (it = adjlist.begin(); it != adjlist.end(); ++it) {
+                double mult = 0;
+
+                //Nested for loop. Outer goes through the columns of the adjacency list
+                for (int i = 0; i < it->second.size(); i++) {
+                    //Goes thru the ranks basically. 
+                    for (int j = 0; j < n; j++) {
+                        if (it->second[i].first == j)
+                            mult += (it->second[i].second * ranks[j]);
+                    }
+                }
+
+                newRanks.push_back(mult);
             }
 
+            //set the rank matrix equal to the newRanks matrix to change values
+            ranks = newRanks;
+            //Clear newRanks matrix to repeat
+            newRanks.clear();
         }
     }
 
+
+    //Now we should have our values, so we go thru and print!
+    //first sort the urls
+    sort(urls.begin(), urls.end(), comparator);
+
+    //Now we print
+    for (int i = 0; i < n; i++) {
+        int currRank = mapper[urls[i]];
+        cout << urls[i] << " " << setprecision(2) << fixed << ranks[currRank] << endl;
+    }
+
 }
 
 
 
-void reMap(int& j, map<string, int>& mapper, vector<dj>& djs, string from, string to) {
-    if (mapper.find(from) == mapper.end())
+void reMap(int& j, map<string, int>& mapper, vector<dj*>& djs, vector<string>& urls, string from, string to) {
+    if (mapper.find(from) == mapper.end()) {
         mapper[from] = j++;
+        dj* toDj = new dj;
+        toDj->toS = from;
+        toDj->d = 0;
+        djs.push_back(toDj);
+        urls.push_back(from);
+    }
 
     if (mapper.find(to) == mapper.end()) {
         mapper[to] = j++;
-        dj toDj;
-        toDj.toS = to;
+        dj* toDj = new dj;
+        toDj->toS = to;
+        toDj->d = 0;
         djs.push_back(toDj);
+        urls.push_back(to);
     }
 
 }
 
-dj findDj(vector<dj> djs, string to) {
-    dj currDj;
-
-    for (int i = 0; i < djs.size(); i++) {
-        if (djs[i].toS == to)
-            currDj = djs[i];
-    }
-
-    return currDj;
-}
 
 
 int main()
 {
-    int n, p; 
-    int j = 0; //j is for the mapper and also the number of urls
     AdjacencyList pages;
     map<string, int> mapper;
-    vector<dj> djs;
+    vector<string> urls;
+    vector<dj*> djs;
+    dj* currDj;
     string from, to;
+
+    
+    int j = 0; //j is for the mapper and also the number of urls
+
+    int n;
     cin >> n;
+
+    int p;
     cin >> p;
+
+    cin.ignore();
+
     for (int i = 0; i < n; i++)
     {
         string line;
         getline(cin, line);
-        istringstream in(line);
+        istringstream in (line);
 
-        string from;
         in >> from;
 
-        string to;
         in >> to;
 
 
-        reMap(j, mapper, djs, from, to);
+        reMap(j, mapper, djs, urls, from, to);
 
         //find the curr dj
-        dj currDj = findDj(djs, to);
-        
-
+        currDj = findDj(djs, to);
+     
+        //insert the edge
         pages.insertEdge(mapper, from, currDj);
-
     }
-    //Create a graph object 
-     pages.PageRank(mapper, p, j);
+    //Run the pagerank
+     pages.PageRank(mapper, urls, p, j);
 
     return 0;
 }
